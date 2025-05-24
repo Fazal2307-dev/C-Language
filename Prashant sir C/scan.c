01592a1737e87177dc61dd84c3d4816114482089
Fazal2307-dev <mdfazal2307@gmail.com>
2025-05-24 10:47:30 +0530

    taking age value

--- Prashant sir C/scan.c
+++ Prashant sir C/scan.c
@@ -3,6 +3,10 @@ int main(){
   int age;
   printf("Please enter you age: ");
   scanf("%d",&age);
-  printf("you are  : %d",age);
+ if(age>= 18){
+  printf("you are eligable for vote");
+ }else{
+  printf("you are not eligable");
+ }
   return 0;
 }
