d46b636912e10360ec892c0086dfe2343fad79f3
Fazal2307-dev <mdfazal2307@gmail.com>
2025-06-02 21:29:02 +0530

    com[leted

--- C Problem/Basic Programming in C/First1.c
+++ C Problem/Basic Programming in C/First1.c
@@ -1,6 +1,14 @@
 #include<stdio.h>
 
 int main(){
+int num1,num2,sum;
+printf("Enter you first num1:");
+scanf("%d",&num1);
+
+printf("Enter you first num2:");
+scanf("%d",&num2);
+sum =num1+num2;
+printf("sum of num1 and num2: %d",sum);
 
 
 
@@ -29,7 +37,5 @@ int main(){
 
 
 
-
-    
     return 0;
 }
