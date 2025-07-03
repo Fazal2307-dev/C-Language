d5ea672fa0c605ac2cc7eb46662b9dd1af5ae6ea
Fazal2307-dev <mdfazal2307@gmail.com>
2025-07-03 10:31:16 +0530

    asad

--- Prashant sir C/2.7.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-int main(){
-  printf("Md\tfazal\n");
-  printf("MD\b\bFazal\n");
-  printf("Md'\fazal\'");
-}
--- Prashant sir C/24/05/2025/calculation.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-  int a,b,x,y,z;
-  printf("please provide value of b x y z:\n");
-  scanf("%d %d %d %d",&b,&x,&y,&z);
- a= z/b+b * x-y;
- printf("Print the value of a:%d",a);
- return 0;
-}
