e1188ed4603507f808b650910e53901cd8d614e3
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-05 20:07:26 +0530

    floating point

--- Bro Code/two.c
+++ Bro Code/two.c
@@ -2,7 +2,9 @@
 int main(){
     int a=5;
     int year = 2025;
+    float gpa =  2.5;
     printf("%d \n",a);
-    printf("this year is %d",year);
+    printf("this year is %d \n",year);
+    printf("your gap is %f \n",gpa);
     return 0;
 }
