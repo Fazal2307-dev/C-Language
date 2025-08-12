9060bbf891f2cb180e2f0a3732e2ef1f6e476f31
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-12 23:49:22 +0530

    const in c

---
+++ GFG C/2.Variable/2.7/const.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+const double PI = 3.14;
+
+int main(){
+// const int x =10;
+// x = x+1;
+// printf("%d",x);
+
+int r;
+printf("Enter value of r \n");
+scanf("%d",&r);
+printf("%f",PI*r*r);
+return 0;
+}
