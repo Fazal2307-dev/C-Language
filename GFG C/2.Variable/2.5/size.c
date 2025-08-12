4815558e04b292fde9875dafb929a9cf3a7fb0bd
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-12 16:21:25 +0530

    using incrementation value if it increase or not

--- GFG C/2.Variable/2.5/size.c
+++ GFG C/2.Variable/2.5/size.c
@@ -3,12 +3,14 @@
 
 int main(){
     int x = 4;
-    double d = 11.5;
-    printf("%zu \n",sizeof(x));
-    printf("%zu \n",sizeof(d));
-    printf("%zu \n",sizeof(15ll));
-    printf("%zu \n",sizeof(13.6f));
-    printf("%zu \n", sizeof(x+10));
+    // double d = 11.5;
+    // printf("%zu \n",sizeof(x));
+    // printf("%zu \n",sizeof(d));
+    // printf("%zu \n",sizeof(15ll));
+    // printf("%zu \n",sizeof(13.6f));
+    // printf("%zu \n", sizeof(x+10));
+    printf("%zu \n",sizeof(x++));
+    printf("%d \n",x);
     return 0;
 
 }
