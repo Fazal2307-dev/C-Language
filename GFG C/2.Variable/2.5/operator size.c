1de7c5ea7fb7a6742c57501754e599fe27b880bd
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-12 16:03:56 +0530

    done

---
+++ GFG C/2.Variable/2.5/operator size.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int x = 4;
+    double d = 11.5;
+    printf("%zu \n",x);
+    printf("%zu \n",d);
+    printf("%zu \n",15ll);
+    printf("%zu \n",13.6f);
+    printf("%zu \n", x+10);
+    return 0;
+
+}
