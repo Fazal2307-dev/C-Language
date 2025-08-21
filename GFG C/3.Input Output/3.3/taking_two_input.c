9fdd85d924283cd0ac7502b80a91be9a7683f46c
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-21 21:17:40 +0530

    taking input in scanf

---
+++ GFG C/3.Input Output/3.3/taking_two_input.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int x,y;
+    printf("Enter value of x and y:\n");
+    scanf("%d %d",&x,&y);
+    printf("Multiplication of x and y is %d",x*y);
+    return 0;
+
+
+}
