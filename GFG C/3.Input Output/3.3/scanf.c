46cdc4055dd16e9d560c329c1786c371fc9cb2a8
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-21 21:08:48 +0530

    scanf()

---
+++ GFG C/3.Input Output/3.3/scanf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int x,y;
+    printf("Enter first value:\n");
+    scanf("%d",&x);
+    printf("Enter second value:\n");
+    scanf("%d",&y);
+    printf("Sum of x and y is %d ",(x+y));
+    return 0;
+}
