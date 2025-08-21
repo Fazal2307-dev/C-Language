823554d1f667bf17fe6fdda92c44b9b5bf5a1734
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-21 23:39:03 +0530

    unsigned,size_t,

--- GFG C/3.Input Output/3.5/Formate_specifier_for_integer.c
+++ GFG C/3.Input Output/3.5/Formate_specifier_for_integer.c
@@ -6,5 +6,10 @@ int main(){
     printf("%ld\n",y);
     long long z = 30;
     printf("%lld\n",z);
+    unsigned w = 40;
+    printf("%u\n",w);
+    size_t v = sizeof(x);
+    printf("%zd",x);
+    
     return 0;
 }
