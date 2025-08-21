7de8af31ea8c3378be53ecc877d8e4bb05b48105
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-21 21:13:05 +0530

    char using scanf()

---
+++ GFG C/3.Input Output/3.3/char_using_scanf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    char name[100];
+    printf("Enter your name:\n");
+    scanf("%s",name);
+    printf("Hi,%s\n",name);
+    printf("Welcome to gfg");
+    return 0;
+
+}
