f1c31a2f519ace930e4b6b71aa7c60ba95eb37a0
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-21 20:58:06 +0530

    printf()

---
+++ GFG C/3.Input Output/3.2/printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    printf("GeeksForGeeks \n");
+    int x = 10;
+    printf("%d\n",x);
+    int y = 20;
+    printf("%d\n",y);
+    printf("Sum of %d and %d is %d",x,y,x+y);
+    return 0;
+
+}
