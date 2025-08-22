93212b95d0905064d18718533c603553df5628d0
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-22 23:26:44 +0530

    airthmetic

---
+++ GFG C/4.Operator/4.1/airthmetic.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int x =12,y=8;
+    printf("%d\n",x +y);
+    printf("%d\n",x -y);
+    printf("%d\n",x *y);
+    printf("%d\n",x /y);
+    printf("%d\n",x %y);
+    return 0;
+}
