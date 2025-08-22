eaacc1a816319a9b041cd30c0eca0c19fd00b044
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-22 19:48:35 +0530

    buffering in c

---
+++ GFG C/3.Input Output/3.9/buffering.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int x,y;
+    printf("Enter first value:\n");
+    scanf("%d",&x);
+    printf("Enter Second value:\n");
+    scanf("%d",&y);
+    printf("Multiplication is %d",x*y);
+    return 0;
+
+
+}
