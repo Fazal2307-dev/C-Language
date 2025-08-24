dec80683905b0d1149756cbe3abc2ac5e8d18ceb
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-24 18:30:58 +0530

    postive negative

---
+++ GFG C/5.Flow Control/5.3/positive_negative.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int number;
+    printf("Enter value of number:\n");
+    scanf("%d",&number);
+    if(number>0)
+    printf("Positive");
+    else if(number<0)
+    printf("Negative");
+    else
+    printf("Zero");
+    return 0;
+}
