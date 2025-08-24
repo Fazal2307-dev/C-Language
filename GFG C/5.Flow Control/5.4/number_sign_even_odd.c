c22ad1c7f79f5606e08ae59b69df167153d0a71e
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-24 18:35:05 +0530

    checking postive value

---
+++ GFG C/5.Flow Control/5.4/number_sign_even_odd.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int number;
+    printf("Enter value of number:\n");
+    scanf("%d",&number);
+    if(number > 0){
+        printf("Postive ");
+        if(number%2==0)
+        printf("Even");
+        else
+        printf("Odd");
+
+    }
+
+
+
+    return 0;
+}
