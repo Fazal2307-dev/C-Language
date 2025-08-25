15dcbb3f0f858ad61ffa099556336dc8f62d27f5
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-25 20:40:16 +0530

    prime factorization

---
+++ GFG C/6.Function/6.9/prime_factorization.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void primeFactor(int n){
+    while(n % 2==0){
+        printf("%d",2);
+        n = n/2;
+    }
+
+    for(int i =3;i*i<=n;i=i+2){
+        while(n % i == 0){
+            printf("%d",i);
+            n = n/i;
+        }
+    }
+    if(n>2)
+    printf("%d",n);
+}
+
+
+int main(){
+    int num;
+    printf("Enter value of num:\n");
+    scanf("%d",&num);
+    printf("Prime Factor:\n");
+    primeFactor(num);
+    return 0;
+}
