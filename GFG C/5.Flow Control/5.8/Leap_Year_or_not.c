32eaf0867e16eceb54b6a0d89f419989afe2e230
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-25 12:15:27 +0530

    add logic in line

--- GFG C/5.Flow Control/5.8/Leap_Year_or_not.c
+++ GFG C/5.Flow Control/5.8/Leap_Year_or_not.c
@@ -3,11 +3,13 @@ int main(){
     int year;
     printf("Enter Year value:\n");
     scanf("%d",&year);
-    if(year % 4==0 && year % 100!=0){
+    if((year % 4==0 && year % 100!=0 ) ||( year % 400==0)){
         printf("Leap Year");
-    }else if(year % 400==0){
-        printf("Leap year");
-    }else
+    }
+    // }else if(year % 400==0){
+    //     printf("Leap year");
+    // }
+    else
     printf("Not a Leap year");
     return 0;
 }
