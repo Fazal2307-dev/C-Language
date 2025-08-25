5f2ab944c26cda9a9b92d7a98dcf32bc15851544
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-25 12:03:51 +0530

    a is bigger

---
+++ GFG C/5.Flow Control/5.7/Largest_of_three_number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("Enter value of a,b and c:\n");
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>b && a>c){
+        printf("A is bigger than all of you");
+
+    }
+
+    return 0;
+}
+
+
