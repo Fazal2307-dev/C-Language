5683dc30730a3edee1e5c18f6cb7cd8c7235a98e
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-25 20:47:09 +0530

    two fun in main()

---
+++ GFG C/6.Function/6.2/two_fun_in_main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void greetMsg(){
+    printf("Hi,\n");
+    printf("Welcome to GFG\n");
+}
+void exitMsg(){
+    printf("Please come to next time:\n");
+}
+int main(){
+greetMsg();
+printf("Hope you are enjoying:\n");
+exitMsg();
+return 0;
+}
