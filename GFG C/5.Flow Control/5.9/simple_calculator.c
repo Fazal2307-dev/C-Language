22c004636e0fd8c0ef798cddc127454e670a4014
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-25 12:25:00 +0530

    simple calculator is

---
+++ GFG C/5.Flow Control/5.9/simple_calculator.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int op,x,y;
+    printf("enter value of op,x and y:\n");
+    scanf("%d %d %d",&op,&x,&y);
+    int res;
+    if(op==1){
+        res = x+y;
+        printf("addition is:%d",res);
+    }else if(op==2){
+        res=x-y;
+        printf("subtraction is:%d",res);
+    }else if(op==3){
+        res = x*y;
+        printf("Multiplication is:%d",res);
+    }else
+    printf("Invalid input");
+    return 0;
+}
