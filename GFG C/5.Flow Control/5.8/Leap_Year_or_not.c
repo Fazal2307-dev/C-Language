e165082a28b9f5ed76f5d3976ff2fd44eafa8798
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-25 12:11:05 +0530

    leap year

---
+++ GFG C/5.Flow Control/5.8/Leap_Year_or_not.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int year;
+    printf("Enter Year value:\n");
+    scanf("%d",&year);
+    if(year % 4==0 && year % 100!=0){
+        printf("Leap Year");
+    }else if(year % 400==0){
+        printf("Leap year");
+    }else
+    printf("Not a Leap year");
+    return 0;
+}
