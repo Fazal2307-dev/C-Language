455341d508cb5cc88a22c3d140e2c4201e356d4a
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-27 10:58:21 +0530

    checking prime no

--- GFG C/7.Loops/7.12/prime.c
+++ GFG C/7.Loops/7.12/prime.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
 int main(){
     int n;
-    printf("enter value of n:\n");
+    printf("Enter value of n:\n");
     scanf("%d",&n);
-    if(n ==0 || n==1){
-        printf("Not a prime number:\n");
+    if(n ==1 || n==0){
+        printf("Not a Prime");
+        return 0;
     }
-    int i=2;
+    int i =2;
     while(i<n){
         if(n % i ==0){
-            printf("Not a prime");
+            printf("Not a prime no");
             return 0;
         }
         i++;
     }
-    printf("Prime No");
+    printf("Prime no");
     return 0;
 }
