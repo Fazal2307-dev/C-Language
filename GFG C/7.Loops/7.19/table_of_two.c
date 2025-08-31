619c9bcf184b1a416daf62abd05001338f808c60
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-31 17:20:06 +0530

    multiplication table

---
+++ GFG C/7.Loops/7.19/table_of_two.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n,m;
+    printf("Enter value of n and m:\n");
+    scanf("%d %d",&n,&m);
+    for(int i=1;i<=m;i++){
+        printf("%d",n*i);
+        printf("\n");
+    }
+    return 0;
+}
