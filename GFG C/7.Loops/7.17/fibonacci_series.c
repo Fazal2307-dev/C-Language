86a3750dda1c368b437eb2b236e2eff22ba6768d
Fazal2307-dev <mdfazal2307@gmail.com>
2025-08-31 17:08:42 +0530

    fibonacci series

---
+++ GFG C/7.Loops/7.17/fibonacci_series.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("Enter value of n:\n");
+    scanf("%d",&n);
+    int a=1;
+    int b=1;
+    int c=0;
+    printf("1 1");
+    for(int i=2;i<=n;i++){
+        c=a+b;
+        printf(" %d ",c);
+        a=b;
+        b=c;
+    }
+    return 0;
+}
