f562403040b264c8fa1e275bd95537d8d65944b9
Fazal2307-dev <mdfazal2307@gmail.com>
2025-09-04 22:23:33 +0530

    swap

--- GFG C/9.Pointers/9.1/swap_two_number.c
+++ GFG C/9.Pointers/9.1/swap_two_number.c
@@ -1,12 +1,15 @@
 #include<stdio.h>
 int main(){
 int a,b,temp=0;
+int *ptr1,*ptr2,*ptr3;
+ptr1=&a;
+ptr2=&b;
 printf("Enter value of a and b:\n");
 scanf("%d %d",&a,&b);
-temp=a;
-a=b;
-b=temp;
-printf("%d %d",a,b);
+temp=*ptr1;
+*ptr1=*ptr2;
+*ptr2=temp;
+printf("After swapping: a = %d, b = %d\n",*ptr1,*ptr2);
 return 0;
 
 }
